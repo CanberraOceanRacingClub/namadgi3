b1440531294ff4e20112412479cd07fdc56cf13b
Steven Ring <smr@southsky.com.au>
2023-03-05 14:43:01 +1100

    Copy notice2skippers to Google Drive

--- src/build_notice_to_skippers.sh
+++ src/build_notice_to_skippers.sh
@@ -14,8 +14,15 @@ OAUTH_KEY=`cat .secrets`
 user=CanberraOceanRacingClub
 repo=namadgi3
 from_date=2099-01-01
-echo $from_date
-echo $OAUTH_KEY
+today=$(date '+%Y-%m-%d')
 cd meeting_minutes
-./gradlew run --args="-u $user -r $repo -o $OAUTH_KEY -f $from_date -l Notice -b -c -n -i -h 'Notice to Skippers' -p /tmp/notice2skippers.pdf"
+echo Generating notice2skippers.pdf
+./gradlew run --args="-u $user -r $repo -o $OAUTH_KEY -f $from_date -l Notice -b -c -n  -h 'Notice to Skippers' -p /tmp/notice2skippers.pdf"
 cd -
+#
+# Copy the notice2skippers.pdf to the Google Drive where it is accessible by all members via a permanent URL
+# https://drive.google.com/file/d/1i-YuaQUBsnXLdzOwMTpy_5Pxi81VKXi4/view?usp=share_link
+#
+echo Copying notice2skippers.pdf to Google Drive
+rclone copy /tmp/notice2skippers.pdf gdrive:Committee\ Area/Subcommittees/Maintenance/Notice2Skippers
+rclone copy /tmp/notice2skippers.pdf gdrive:Committee\ Area/Subcommittees/Maintenance/Notice2Skippers/history/notice2skippers_$today.pdf
